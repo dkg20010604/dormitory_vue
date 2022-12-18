a5c06de976ea4e43364aa4c1f9bbcd4055e83a52
dkg20010604 <858943985@qq.com>
2022-12-18 22:02:18 +0800

    创建项目引入依赖

---
+++ vite.config.ts
@@ -0,0 +1,32 @@
+import { fileURLToPath, URL } from "node:url";
+
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import vueJsx from "@vitejs/plugin-vue-jsx";
+
+import AutoImport from 'unplugin-auto-import/vite';
+import Components from 'unplugin-vue-components/vite';
+import { TDesignResolver } from 'unplugin-vue-components/resolvers';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    vueJsx(),
+    AutoImport({
+      resolvers: [TDesignResolver({
+        library: 'vue-next'
+      })],
+    }),
+    Components({
+      resolvers: [TDesignResolver({
+        library: 'vue-next'
+      })],
+    }),
+  ],
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+});
