eebcacd4de132972734dbe412d1014db12ffc068
dkg20010604 <858943985@qq.com>
2022-12-18 22:16:41 +0800

    创建项目

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,13 @@
 import { createApp } from "vue";
 import { createPinia } from "pinia";
-
+import 'tdesign-vue-next/es/style/index.css';
 import App from "./App.vue";
 import router from "./router";
-
 import "./assets/main.css";
-
+import store from './stores'
 const app = createApp(App);
 
 app.use(createPinia());
 app.use(router);
-
+app.use(store);
 app.mount("#app");
