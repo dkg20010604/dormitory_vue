eaeaece3881e2155896fa0c28e6d89bd352b85be
天宏 王 <1066088568@qq.com>
2022-12-19 17:23:13 +0800

    添加teacher_query文件夹

--- components.d.ts
+++ components.d.ts
@@ -7,23 +7,7 @@ export {}
 
 declare module '@vue/runtime-core' {
   export interface GlobalComponents {
-    HelloWorld: typeof import('./src/components/HelloWorld.vue')['default']
-    IconCommunity: typeof import('./src/components/icons/IconCommunity.vue')['default']
-    IconDocumentation: typeof import('./src/components/icons/IconDocumentation.vue')['default']
-    IconEcosystem: typeof import('./src/components/icons/IconEcosystem.vue')['default']
-    IconSupport: typeof import('./src/components/icons/IconSupport.vue')['default']
-    IconTooling: typeof import('./src/components/icons/IconTooling.vue')['default']
-    LoginComponent: typeof import('./src/components/LoginComponent.vue')['default']
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
-    TButton: typeof import('tdesign-vue-next')['Button']
-    TCheckbox: typeof import('tdesign-vue-next')['Checkbox']
-    TForm: typeof import('tdesign-vue-next')['Form']
-    TFormItem: typeof import('tdesign-vue-next')['FormItem']
-    TheWelcome: typeof import('./src/components/TheWelcome.vue')['default']
-    TIcon: typeof import('tdesign-vue-next')['Icon']
-    TInput: typeof import('tdesign-vue-next')['Input']
-    TSpace: typeof import('tdesign-vue-next')['Space']
-    WelcomeItem: typeof import('./src/components/WelcomeItem.vue')['default']
   }
 }
